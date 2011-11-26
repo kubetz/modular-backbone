f10bf141db9a1cb277aa809850460abe33d36029
Jakub Kozisek <jakub.kozisek@gmail.com>
2011-11-26 10:10:38 +0100

    renamed libs to contain version numbers

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@
 // Their usage will become more apparent futher along in the tutorial.
 require.config({
   paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-optamd3-min',
-    text: 'libs/require/text',
+    jquery: 'libs/jquery/jquery-1.7.1.min',
+    underscore: 'libs/underscore/underscore-1.2.2.min',
+    backbone: 'libs/backbone/backbone-0.5.3_optamd3.min',
+    text: 'libs/require/text-0.27.0.min',
     templates: '../templates'
   }
 
